7440705e69ab0f7a4b3790e563ee08b677f10090
daenv <daenvle69@gmail.com>
2024-03-22 12:27:59 +0700

    update 22/3

---
+++ Overloading-cs/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Overloading
+{
+    class Box
+    {
+        public double height;
+        public double breath;
+        public double width;
+        
+        
+        
+    }
+};
+
